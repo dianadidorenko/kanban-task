f80eb15333e649d1bf6837ea799b79e728f0daef
Диана <diana@MacBook-Pro-Diana.local>
2026-06-27 14:21:59 +0300

    fix 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import ListContainer from "@/components/ListContainer";
 import prisma from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const lists = await prisma.list.findMany({
     include: {
